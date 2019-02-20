05626d4a39958cfd2e71097de6f23880cf85c50a
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 10:28:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,17 @@ class Student
     end
   end
 
-
+  def self.first_X_students_in_grade_10(n)
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        LIMIT ?
+      SQL
+      DB[:conn].execute(sql, n).map do |row|
+        self.new_from_db(row)
+      end
+    end
 
   def self.first_student_in_grade_10
     sql = <<-SQL
