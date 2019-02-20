0294ae5b4c9b62e19a2b4292198d7c72e30bd4b9
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 10:07:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT x
+      LIMIT = x
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
