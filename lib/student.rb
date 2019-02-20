2eb0339bef32febf0be3f160bf0be8f7c8aec10a
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 10:03:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,9 @@ class Student
       FROM students
       WHERE grade = 9
     SQL
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
