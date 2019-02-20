0f759e15e5b724f503ab4950117cc58be5f0898d
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 10:03:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,15 +21,15 @@ class Student
   end
 
   def self.all_students_in_grade_9
-    sql = <<-SQL 
-    SELECT * 
-    FROM students 
+    sql = <<-SQL
+    SELECT *
+    FROM students
     WHERE grade = 9
-    SQL 
-    DB[:conn].execute(sql).map do |row| 
+    SQL
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    end 
+    end
 
   def self.find_by_name(name)
     sql = <<-SQL
