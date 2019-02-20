d53a1cf9310ee5a3e044d25cda2a9465af438c89
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 10:23:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,7 @@ class Student
       FROM students
       WHERE grade = 10
       LIMIT = ?
+      ORDER BY students.id
     SQL
     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
